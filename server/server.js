7ff42cf9dcc05b7b760a67fbdb581c726a255e58
Trang Truong <trangldtruong@gmail.com>
2024-02-13 03:37:21 -0500

    implement Apollo

--- server/server.js
+++ server/server.js
@@ -5,17 +5,16 @@ const { expressMiddleware } = require('@apollo/server/express4');
 
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
 
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-const app = express();
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
@@ -28,10 +27,10 @@ app.use('/graphql', expressMiddleware(server));
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  })
 }
-
-app.use(routes);
-
 
 db.once('open', () => {
   app.listen(PORT, () => {
